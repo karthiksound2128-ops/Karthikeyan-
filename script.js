ea55b9faa7bbfbbd6b746e73df6f6f5d4252dfa9
Vignesh <karthiksound2128@gmail.com>
2025-09-09 14:40:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Dark / Light Mode Toggle
+const toggleBtn = document.getElementById("theme-toggle");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    toggleBtn.textContent = "☀️";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
+});
